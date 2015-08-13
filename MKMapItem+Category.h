889a63ac71ff7aa2e9ccfeabcbc53b86564f8a2b
Alex Blair <alexblair190@gmail.com>
2015-08-12 18:40:58 -0600

    test

---
+++ MKMapItem+Category.h
@@ -0,0 +1,13 @@
+//
+//  MKMapItem+Category.h
+//  Bait
+//
+//  Created by Stephen Blair on 8/12/15.
+//  Copyright (c) 2015 Stephen Blair. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface MKMapItem (Category)
+
+@end
---
+++ MKMapItem+Category.m
@@ -0,0 +1,13 @@
+//
+//  MKMapItem+Category.m
+//  Bait
+//
+//  Created by Stephen Blair on 8/12/15.
+//  Copyright (c) 2015 Stephen Blair. All rights reserved.
+//
+
+#import "MKMapItem+Category.h"
+
+@implementation MKMapItem (Category)
+
+@end
